a86c1d2ca70098e0c89e1b7125a9be9439c3ee8b
NoemiCorsi96 <noemicorsi96@gmail.com>
2026-01-20 17:50:42 +0100

    Use Bootstrap for the layout and display each disc in a card

--- index.php
+++ index.php
@@ -17,21 +17,39 @@ $dischi = json_decode($jsonText, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-DISCHI</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <h1>Lista dischi</h1>
-<!-- Ciclo sui dischi e li stampo -->
-  <?php foreach ($dischi as $disco) { ?>
-    <div style="margin-bottom: 20px;">
-      <h2><?php echo $disco['titolo']; ?></h2>
-      <p>
-        Artista: <?php echo $disco['artista']; ?><br>
-        Anno: <?php echo $disco['anno']; ?><br>
-        Genere: <?php echo $disco['genere']; ?>
-      </p>
-      <img src="<?php echo $disco['cover']; ?>" alt="<?php echo $disco['titolo']; ?>" width="150">
-      <hr>
+   <body class="bg-light">
+
+  <div class="container py-4">
+    <h1 class="text-center mb-4">Lista Dischi</h1>
+
+    <div class="row">
+      <?php foreach ($dischi as $disco) { ?>
+        <div class="col-12 col-md-4 mb-4">
+          <div class="card h-100">
+            <img 
+              src="<?php echo $disco['cover']; ?>" 
+              class="card-img-top" 
+              alt="<?php echo $disco['titolo']; ?>"
+            style="height: 350px; object-fit: cover;"
+            >
+
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $disco['titolo']; ?></h5>
+              <p class="card-text">
+                <strong>Artista:</strong> <?php echo $disco['artista']; ?><br>
+                <strong>Anno:</strong> <?php echo $disco['anno']; ?><br>
+                <strong>Genere:</strong> <?php echo $disco['genere']; ?>
+              </p>
+            </div>
+          </div>
+        </div>
+      <?php } ?>
     </div>
-  <?php } ?>
+
+  </div>
 </body>
 </html>
